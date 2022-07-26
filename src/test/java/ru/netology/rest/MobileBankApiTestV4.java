5b583994f41cb1e27fa77ea99b2b16d02d90b22c
nickolichev@gmail.com <nickolichev@gmail.com>
2022-07-26 18:25:23 +0300

    modification accounts.schema.json test successeful

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,9 +1,15 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.patch;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV4 {
   @Test
@@ -18,7 +24,6 @@ class MobileBankApiTestV4 {
             // Проверки
             .then()
             .statusCode(200)
-            .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-    ;
+            .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
   }
 }
